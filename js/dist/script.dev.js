e1997ec035a508c6cc3a4f1e7efaf9f33e7f9d2a
Viktor <ololoshe4kaaaaaa@gmail.com>
2022-04-14 17:36:46 +0300

    first commit

---
+++ js/dist/script.dev.js
@@ -0,0 +1,12 @@
+"use strict"; //const result = confirm('Are you here?'); //
+//console.log(result);
+//const answer = prompt('Do you 19?', '19');
+//console.log(answer + 5);
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+var answers = [];
+answers[0] = prompt('h u name?', '');
+answers[1] = prompt('h u name mother?', '');
+answers[2] = prompt('h u name f name?', '');
+console.log(_typeof(answers));
---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+
+//const result = confirm('Are you here?'); //
+//console.log(result);
+
+//const answer = prompt('Do you 19?', '19');
+//console.log(answer + 5);
+
+
+const answers = [];
+
+answers[0] = prompt('h u name?', '');
+answers[1] = prompt('h u name mother?', '');
+answers[2] = prompt('h u name f name?', '');
+
+console.log(typeof(answers));
+
+
