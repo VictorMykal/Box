0682fbc819bd5845c7e3eb2deee32dcfe35a6adf
Viktor <ololoshe4kaaaaaa@gmail.com>
2022-04-14 21:08:34 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-
-"use strict";
-
-
-//const result = confirm('Are you here?'); //
-//console.log(result);
-
-//const answer = prompt('Do you 19?', '19');
-//console.log(answer + 5);
+const numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
 
 
-const answers = [];
+const personalMovieDB = {
+    count:  numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 
-answers[0] = prompt('h u name?', '');
-answers[1] = prompt('h u name mother?', '');
-answers[2] = prompt('h u name f name?', '');
 
-console.log(typeof(answers));
+};
+
+
+
+const  a = prompt('Один з останніх переглянутих фільмів?', ''),
+       b = prompt('На скільки оціните його?', ''),
+       c = prompt('Один з останніх переглянутих фільмів?', ''),
+       d = prompt('На скільки оціните його?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
 
 
