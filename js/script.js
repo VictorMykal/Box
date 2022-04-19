12f29c8becc9fc197ebe3c7e1a3ef89a53bb5ad5
Viktor <ololoshe4kaaaaaa@gmail.com>
2022-04-19 14:39:52 +0300

    lol

--- js/script.js
+++ js/script.js
@@ -13,14 +13,37 @@ const personalMovieDB = {
 
 
 
-const  a = prompt('Один з останніх переглянутих фільмів?', ''),
-       b = prompt('На скільки оціните його?', ''),
-       c = prompt('Один з останніх переглянутих фільмів?', ''),
-       d = prompt('На скільки оціните його?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
+
+// Цикли
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один з останніх переглянутих фільмів?', ''),
+          b  = prompt('На скільки оціните його?', '');
+
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b; 
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+   
+}
+
+
+if (personalMovieDB.count < 10) {
+    console.log('Переглянуто доволі мало');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Ви класичний глядач');
+} else if (personalMovieDB.count >= 30){
+    console.log('Ви кіноман');
+} else {
+    console.log('Помилка');
+}
 
 console.log(personalMovieDB);
 
