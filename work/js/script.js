939ba99062ebd46d74e53c95c10f811db88eb5a0
viktor <ololoshe4kaaaaaa@gmail.com>
2022-05-15 12:54:56 +0300

    first commit

---
+++ work/js/script.js
@@ -0,0 +1,133 @@
+"use sctrict";
+
+
+
+
+/* let number = 5;
+const leftBorderWidth = 1; 
+
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+
+};
+
+obj.a = 10;
+console.log(obj);
+
+
+var name = 'Vitya'; */
+
+
+                /*   Правопис */
+
+
+
+/* const venicleBodyWidth = 5000;
+const venicleBodyLenght = 4000;
+ */
+//request
+//data
+//response
+
+// snake_case
+// UPPER_SNAKE_CASE
+
+// const COLOR_RED = '#F00'
+
+// kebab-case
+// PascaleCase
+                        
+
+                          /* ТИпи данних */
+
+/* let number = 4 ;
+let nNmber = 4.6 ;
+
+const persone = 'Vova'; - строка */
+
+/* const bool = true or false */
+
+
+/* const obj = {
+    name: 'Vova',
+    age: 25,
+    isMarried: false
+};
+
+
+console.log(obj.name);
+ */
+
+
+        /*         0           1             2   3   4   5
+
+let arr = ['plum.png', 'lol.jpg', 'aple.bpb', 6, {}, []];
+ */
+
+
+
+
+
+                   /*  Різниця між обектом і масивом */
+
+
+
+/* const arr = ['a', 'b', 'c'];
+
+const arrobj = {
+    0: 'a',
+    1: 'b',
+    2: 'c'
+};
+
+ */
+
+/* arrobj.b = '1234';
+
+/* console.log(arr[1]); */
+/* console.log(arrobj[b]);
+ */
+
+/* const obj = {          обект
+     
+    Anna: 500,
+    'Alice': 800
+
+
+};
+
+ */
+
+
+                 /*  Обращение с пользувателем */
+
+
+/* const result = confirm('Are you here');
+console.log(result);
+ */
+
+
+
+/* const answer = prompt('Are you 18', '18');
+console.log(typeof(answer)); */
+
+/* 
+const answer = +prompt('Are you 18', '18');
+console.log(answer + 5); */
+
+/* 
+const answers = [];
+
+answers[0] = prompt('W is u name?', '');
+answers[1] = prompt('W is u lastname?', '');
+answers[2] = prompt('W is u mother name?', '');
+
+console.log(typeof(answers)); */
+
+
+
+1111
